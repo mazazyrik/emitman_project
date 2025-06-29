8da60e87ce66e1a0b48f2e5681b7250686659fd6
mazazyrik <117200062+mazazyrik@users.noreply.github.com>
2025-06-29 22:53:26 +0300

    backend: async

--- code/backend/routers/decanat.py
+++ code/backend/routers/decanat.py
@@ -7,8 +7,8 @@ router = APIRouter(prefix="/api/decanat", tags=["decanat"])
 
 
 @router.get("/all")
-def get_decanats() -> DecanatGroupGetSchema:
-    all_decs = Decanat.all().prefetch_related('groups')
+async def get_decanats() -> DecanatGroupGetSchema:
+    all_decs = await Decanat.all().prefetch_related('groups')
 
     decs_with_groups_list = [
         DecanatGroupGetSchema(
@@ -21,8 +21,8 @@ def get_decanats() -> DecanatGroupGetSchema:
 
 
 @router.get("/all/{decanat_name}")
-def get_decanat_with_group(decanat_name: str) -> DecanatGroupGetSchema:
-    dec = Decanat.get(name=decanat_name)
+async def get_decanat_with_group(decanat_name: str) -> DecanatGroupGetSchema:
+    dec = await Decanat.get(name=decanat_name)
     return DecanatGroupGetSchema(
         name=dec.name,
         groups=[group.name for group in dec.groups]
@@ -30,8 +30,8 @@ def get_decanat_with_group(decanat_name: str) -> DecanatGroupGetSchema:
 
 
 @router.get('/{decanat_name}')
-def get_decanat(decanat_name: str) -> DecanatGetSchema:
-    dec = Decanat.get(name=decanat_name).prefetch_related('teachers')
+async def get_decanat(decanat_name: str) -> DecanatGetSchema:
+    dec = await Decanat.get(name=decanat_name).prefetch_related('teachers')
 
     return DecanatGetSchema(
         name=dec.name,
